10c391029f2b991d10747adcab7ee7a0134eebb0
GabrielJared <gabrieljaredjared@gmail.com>
2022-05-31 18:53:08 -0300

    2 - programa ainda com erros

--- Features/Step_def/failureLoginSteps.js
+++ Features/Step_def/failureLoginSteps.js
@@ -1,24 +1,25 @@
-const {Given, When, Then} = require ('@cucumber/cucumber');
-const {expect} = require('chai');
-const {By} = require("selenium-webdriver");
-const {setDefaultTimeout}= require('@cucumber/cucumber');
-const {initDriver} = require('../Support/driveUtil');
-const {After, Before} = require('@cucumber/cucumber');
-setDefaultTimeout(60*1000);
+const {Given, When, Then, After, Before} = require ('@cucumber/cucumber'); //carrega os modulos GIVEN WHEN THEN, After e Before do cucumber para informar as açoes tomadas antes e depois de executar as etapas da feature
+const {expect} = require('chai'); //carrega o modulo expect do chai, para validar o resultado 
+const {By} = require("selenium-webdriver"); // '' By do selenium-webdriver para informar o tipo de dado esperado 
+const {setDefaultTimeout}= require('@cucumber/cucumber'); // '' setDefaultTimeout para limitar o tempo de espera do driver
+const {initDriver} = require('../Support/driveUtil');// '' iniDriver do arquivo driveUtil para construir o driver
+setDefaultTimeout(60*1000); //seta para 1 minuto o tempo de espera maximo do driver
 
 let driver;
+//inicia o driver 
 Before(function(){
     this.driver = initDriver();
 });
-
+//fecha o driver
 After(function(){
     this.driver.quit();
 });
 
-When('informar algum dado incorretamente', function () {
-    
+When('informar algum dado incorretamente', async() => {
+    await driver.findElement(By.css('input[name=email]')).sendKeys('ASDASDASDASD@gmail.com'+'\n');
 });
 
-Then('o login nao poderá ser realizado', function () {
-    
+Then('o login nao poderá ser realizado', async() => {
+    let text = await driver.findElement(By.xpath('//*[@id="center_column"]/div[1]/ol/li')).getText();
+    expect(text).equal("Authentication failed.");
 });
--- Features/Step_def/successLoginSteps.js
+++ Features/Step_def/successLoginSteps.js
@@ -1,28 +1,32 @@
-const {Given, When, Then} = require ('@cucumber/cucumber');
-const {expect} = require('chai');
-const {By} = require("selenium-webdriver");
-const {setDefaultTimeout}= require('@cucumber/cucumber');
-const {initDriver} = require('../Support/driveUtil');
-const {After, Before} = require('@cucumber/cucumber');
-setDefaultTimeout(60*1000);
+const {Given, When, Then, After, Before} = require ('@cucumber/cucumber'); //carrega os modulos GIVEN WHEN THEN, After e Before do cucumber para informar as açoes tomadas antes e depois de executar as etapas da feature
+const {expect} = require('chai'); //carrega o modulo expect do chai, para validar o resultado 
+const {By} = require("selenium-webdriver"); // '' By do selenium-webdriver para informar o tipo de dado esperado 
+const {setDefaultTimeout}= require('@cucumber/cucumber'); // '' setDefaultTimeout para limitar o tempo de espera do driver
+const {initDriver} = require('../Support/driveUtil');// '' iniDriver do arquivo driveUtil para construir o driver
+setDefaultTimeout(60*1000); //seta para 1 minuto o tempo de espera maximo do driver
 
 let driver;
+//inicia o driver 
 Before(function(){
-    this.driver = initDriver();
+    driver = initDriver();
 });
 
+//fecha o driver
 After(function(){
-    this.driver.quit();
+    driver.quit();
 });
 
 Given('que tenha acesso ao site', async() => {
-    await driver.get("http://automationpractice.com/index.php");
+    await driver.get("http://automationpractice.com/index.php"); //url do site
+    driver.findElement(By.xpath('//*[@id="header"]/div[2]/div/div/nav/div[1]/a')).click; //botao de login
 });
 
-When('informar os dados corretamente', function () {
-    
-});
+When('informar os dados corretamente', async() => {
+    let email = await driver.findElement(By.xpath('//*[@id="email"]/div/div[1]/a')).sendKeys('gabrieljaredjared@gmail.com'+'\n');
+    await driver.findElement(By.css('input[id=passwd]')).sendKeys('99743461'+'\n');
+}); 
 
-Then('o login deve ser realizado com sucesso', function () {
-    
+Then('o login deve ser realizado com sucesso', async() => {
+    let text = await driver.findElement(By.className('page-heading')).getText();
+    expect(text).equal("Order history and details");
 });
